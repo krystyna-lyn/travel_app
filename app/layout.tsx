69a95160aeba8ea1718fb409bea0b5b5223356fb
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-11-14 00:18:40 +0000

    update files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css'
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
 
 export const metadata: Metadata = {
   title: 'Travel Guide',
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
+        <Hero/>
         <main className='relative overflow-hidden'>
           {children}
         </main>
--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,48 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/', key: 'how_it_works', label: 'How it Works?' },
-    { href: '/', key: 'services', label: 'Services' },
-    { href: '/', key: 'pricing ', label: 'Pricing ' },
-    { href: '/', key: 'contact_us', label: 'Contact Us' },
-  ];
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_web_work', label: 'How our Website Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+
+// FOOTER 
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Web',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing', 'Hiking', 'kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '+49 23457890' },
+    { label: 'Email Officer', value: 'travel@web.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
+
